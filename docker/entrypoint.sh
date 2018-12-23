5710b72f577a8463154a1517980ceb1cc31ccaec
Diego Ferigo <dgferigo@gmail.com>
2018-12-23 10:32:27 +0100

    Added docker image

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [ ! -x "$(which setup_devenv.sh)" ] ; then
+    echo "File setup_devenv.sh not found."
+    exit 1
+fi
+
+# Initialize the container
+setup_devenv.sh
+echo "==> Devenv container ready"
+
+# If a CMD is passed, execute it
+exec "$@"
---
+++ docker/setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+# These variables can be overridden by docker environment variables
+USER_UID=${USER_UID:-1000}
+USER_GID=${USER_GID:-1000}
+USERNAME=${USERNAME:-docker}
+
+create_user() {
+	# If the home folder exists, set a flag.
+	# Creating the user during container initialization often is anticipated
+	# by the mount of a docker volume. In this case the home directory is already
+	# present in the file system and adduser skips by default the copy of the
+	# configuration files.
+	HOME_FOLDER_EXISTS=0
+	if [ -d /home/$USERNAME ] ; then HOME_FOLDER_EXISTS=1 ; fi
+
+	# Create a group with USER_GID
+	if ! getent group ${USERNAME} >/dev/null; then
+		echo "Creating ${USERNAME} group"
+		groupadd -f -g ${USER_GID} ${USERNAME} 2> /dev/null
+	fi
+
+	# Create a user with USER_UID
+	if ! getent passwd ${USERNAME} >/dev/null; then
+		echo "Creating ${USERNAME} user"
+		adduser --quiet \
+		        --disabled-login \
+				  --uid ${USER_UID} \
+				  --gid ${USER_GID} \
+				  --gecos 'Workspace' \
+				  ${USERNAME}
+	fi
+
+	# If configuration files have not been copied, do it manually
+	if [ ! $HOME_FOLDER_EXISTS -eq 0 ] ; then
+		chown ${USER_UID}:${USER_GID} /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bashrc /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bash_logout /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.profile /home/${USERNAME}
+	fi
+}
+
+# Create the user
+echo "==> Creating the runtime user"
+create_user
+
+# Set a default root password
+echo "==> Setting the default root password"
+ROOT_PASSWORD="root"
+echo "root:${ROOT_PASSWORD}" | chpasswd
+
+# Set a default password
+echo "==> Setting the default user password"
+USER_PASSWORD=${USERNAME}
+echo "${USERNAME}:${USER_PASSWORD}" | chpasswd
+echo "${USERNAME}    ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+# Add the user to video group for HW acceleration (Intel GPUs)
+usermod -aG video ${USERNAME}
+
+# TODO: setup git
